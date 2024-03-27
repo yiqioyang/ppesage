1e601f1c1603305e33c20dd2d484b68029588528
yiqioyang <yiqioyang3@gmail.com>
2024-03-27 11:12:47 -0400

    Get ready to delete some functions that are not currently used

--- application/training.R
+++ application/training.R
@@ -5,7 +5,7 @@ fun_names = list.files("~/Documents/code_proj/simp_add_gp/functions/" ,full.name
 apply(X = matrix(fun_names, ncol = 1), MARGIN = 1, FUN = source)
 
 ###########
-out_ind = 36
+out_ind = 3
 inp = to_hypercube(rbind(inp_a, inp_b))
 out = rbind(out_a, out_b)
 df = cbind(inp, y = to_gau01(out[, out_ind]))
@@ -29,13 +29,14 @@ dftrn_comb = rbind(dftrn, dftrn_val)
 dftst = df[-trn_ind,]
 
 ########################
-l1_meta_a = seq_gaus_1d_b(x = dftrn[,1:n_inp], y = dftrn$y, range = 0.6, nugget = 4, iteration = 20)
-l1_calc_a = apply_emu(x = dftrn[,1:n_inp], y = dftrn$y, meta_data = l1_meta_a)
-l1_meta_b = seq_gaus_1d_b(x = dftrn[,1:n_inp], y = l1_calc_a[[2]], range = 0.6, nugget = 4, iteration = 10)
-l1_calc_b = apply_emu(x = dftrn[,1:n_inp], y = l1_calc_a[[2]], meta_data = l1_meta_b)
+#l1_meta_a = seq_gaus_1d_b(x = dftrn[,1:n_inp], y = dftrn$y, range = 0.6, nugget = 4, iteration = 20)
+#l1_calc_a = apply_emu(x = dftrn[,1:n_inp], y = dftrn$y, meta_data = l1_meta_a)
+#l1_meta_b = seq_gaus_1d_b(x = dftrn[,1:n_inp], y = l1_calc_a[[2]], range = 0.6, nugget = 4, iteration = 10)
+#l1_calc_b = apply_emu(x = dftrn[,1:n_inp], y = l1_calc_a[[2]], meta_data = l1_meta_b)
 
 #Or
-#l1_meta = seq_gaus_1d_a(x = dftrn[,1:n_inp], y = dftrn$y, range = 0.6, nugget = 4, iteration = 20)
+#l1_meta = seq_gaus_1d_a(x = dftrn[,1:n_inp], y = dftrn$y, range = 0.6, nugget = 4, iteration = 30)
+
 #########################
 l2_meta = seq_gaus_2d(x = dftrn[,1:n_inp], y = l1_calc_b[[2]], range = c(0.5, 0.5), nugget = 4, var_nm = inp_nm, top_n = 45)[[1]]
 l2_calc = apply_emu(x = dftrn[,1:n_inp], y = l1_calc_b[[2]], meta_data = l2_meta)
@@ -45,7 +46,6 @@ l3_meta = seq_gaus_3d(x = dftrn[,1:n_inp], y = l2_calc[[2]], range = c(0.4, 0.4,
 comb_meta = rbind(as.matrix(l1_meta_a), as.matrix(l1_meta_b), 
                   as.matrix(l2_meta), as.matrix(l3_meta))
 #########
-
 y_trn_val = apply_emu_val(dftrn[,1:n_inp], dftrn$y, meta_data = comb_meta, xtst = dftrn_val[,1:45])
 plot(apply(dftrn_val$y - y_trn_val[[3]], MARGIN = 2, sd)/sd(dftrn_val$y), 
      ylim = c(0, 1), xlab = "No of iteration", ylab = "Sd at each iteration", pch = 16, cex = 0.8)
@@ -53,8 +53,7 @@ abline(v = (1:(nrow(comb_meta)/5))*5 , col = "gray", lty = 2)
 abline(v = (1:(nrow(comb_meta)/10))*10 , col = "gray")
 
 
-
-comb_meta_update = comb_meta[c(1:20, 21:25, 31:40),]
+comb_meta_update = comb_meta[c(1:65),]
 ################################
 y_pred_origin = apply_emu_val(dftrn_comb[,1:n_inp], dftrn_comb$y, meta_data = comb_meta, xtst = dftst[,1:n_inp])
 y_pred_update = apply_emu_val(dftrn_comb[,1:n_inp], dftrn_comb$y, meta_data = comb_meta_update, xtst = dftst[,1:n_inp])
@@ -64,6 +63,8 @@ plot(apply(dftrn_val$y - y_trn_val[[3]], MARGIN = 2, sd)/sd(dftrn_val$y),
      ylim = c(0, 1), xlab = "No of iteration", ylab = "Sd at each iteration", pch = 16, cex = 0.8)
 points(apply(dftst$y - y_pred_origin[[3]], MARGIN = 2, sd)/sd(dftst$y), col = "seagreen", pch = 16, cex = 0.8) 
 lines(apply(dftst$y - y_pred_update[[3]], MARGIN = 2, sd)/sd(dftst$y), col = "indianred2")
+
+
 points(apply(dftst$y - y_pred_update[[3]], MARGIN = 2, sd)/sd(dftst$y), col = "indianred2", pch = 17)
 abline(v = (1:(nrow(comb_meta)/5))*5 , col = "gray", lty = 2)
 abline(v = (1:(nrow(comb_meta)/10))*10 , col = "gray")
