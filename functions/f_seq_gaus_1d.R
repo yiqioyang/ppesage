d325680dd6de9925f6884c25110d59257f230f42
yiqioyang <yiqioyang3@gmail.com>
2024-06-26 17:59:44 -0400

    Update functions for better visualization such that it is consistent with manuscript. Need to check

--- functions/f_seq_gaus_1d.R
+++ functions/f_seq_gaus_1d.R
@@ -17,9 +17,18 @@ seq_gaus_1d_a <- function(x = x, y = y,  range, nugget, iteration, var_nm = coln
     
     temp_res = apply(data.frame(dims = 1:ncol_x), MARGIN = 1, FUN = auto_gaus_sel_ind, y = pre_y, range = range, nugget = nugget, x = x)
     temp_ind = which(temp_res == min(temp_res))[1]
+    
+    if(i == 1 | i %% 10 == 1 ){
+      initial_res = temp_res
+    }
+    
     print(var_nm[temp_ind])
-    if(i == 1 | i %% 5 == 0 | i == iteration){
-      plot(temp_res, ylim = c(min(temp_res - 0.05),1), xlab = i)
+    if(i == 1 | i %% 10 == 1 ){
+      plot(initial_res, ylim = c(0,1.1), xlab = "Para No", ylab = "Res at each iteration")
+      abline(v = temp_ind, col = "black", lty = 2)
+    }else{
+     lines(temp_res, col = i) 
+      abline(v = temp_ind, col = i, lty = 2)
     }
     sel_ind[i] = temp_ind   #xxx
     temp_pred_obj = rgasp(design = x[,temp_ind], response = pre_y, nugget = nugget, range.par = range)
--- functions/plotting.R
+++ functions/plotting.R
@@ -11,30 +11,34 @@ plot_val <- function(pred_adding, ytrue){
 
 
   
-plot_tst <- function(tst_pred_long, tst_pred_short, ytrue){
+plot_tst <- function(tst_pred_long, y_val, tst_pred_short, tst_pred_tst, ytrue){
   
-  plot(apply(tst_pred_long[[3]] - ytrue, MARGIN = 2, sd)/sd(ytrue), 
+  plot(apply(tst_pred_long[[3]] - y_val, MARGIN = 2, sd)/sd(y_val), 
        ylim = c(0, 1), xlab = "No of iteration", ylab = "Sd at each iteration", pch = 16, cex = 0.8)
   
-  points(apply(tst_pred_long[[3]] - ytrue, MARGIN = 2, sd)/sd(ytrue), col = "seagreen", pch = 16, cex = 0.8) 
   
-  lines(apply(tst_pred_short[[3]] - ytrue, MARGIN = 2, sd)/sd(ytrue), col = "indianred2")
+  points(apply(tst_pred_short[[3]] - ytrue, MARGIN = 2, sd)/sd(ytrue), col = "seagreen", pch = 17)
+  lines(apply(tst_pred_short[[3]] - ytrue, MARGIN = 2, sd)/sd(ytrue), col = "seagreen")
   
-  points(apply(tst_pred_short[[3]] - ytrue, MARGIN = 2, sd)/sd(ytrue), col = "indianred2", pch = 17)
+  
+  points(apply(tst_pred_tst[[3]] - ytrue, MARGIN = 2, sd)/sd(ytrue), col = "indianred2", pch = 17)
+  lines(apply(tst_pred_tst[[3]] - ytrue, MARGIN = 2, sd)/sd(ytrue), col = "indianred2")
+  
+  
   abline(v = (1:(nrow(comb_meta)/5))*5 , col = "gray", lty = 2)
   abline(v = (1:(nrow(comb_meta)/10))*10 , col = "gray")
   
   
-  rel_sd1 = sd(tst_pred_long[[1]] - ytrue)/sd(ytrue)
-  rel_sd2 = sd(tst_pred_short[[1]] - ytrue)/sd(ytrue)
+  rel_sd1 = sd(tst_pred_short[[1]] - ytrue)/sd(ytrue)
+  rel_sd2 = sd(tst_pred_tst[[1]] - ytrue)/sd(ytrue)
   
   
-  r2_1 = 1 - sum((tst_pred_long[[1]] - ytrue)^2) / sum((ytrue - mean(ytrue))^2)
-  r2_2 = 1 - sum((tst_pred_short[[1]] - ytrue)^2) / sum((ytrue - mean(ytrue))^2)
+  r2_1 = 1 - sum((tst_pred_short[[1]] - ytrue)^2) / sum((ytrue - mean(ytrue))^2)
+  r2_2 = 1 - sum((tst_pred_tst[[1]] - ytrue)^2) / sum((ytrue - mean(ytrue))^2)
   
   output = matrix(c(rel_sd1, rel_sd2, r2_1, r2_2), nrow = 2)
   colnames(output) = c("rel_sd", "r2")
-  rownames(output) = c("long", "short")
+  rownames(output) = c("short", "tst")
   return(output)
 }
 
