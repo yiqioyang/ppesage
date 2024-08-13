fb4e168525dda40749125853a621d9d6422ed961
yiqioyang <yiqioyang3@gmail.com>
2024-08-13 16:36:08 -0400

    Add extra lines to save figures and diagnostic results

--- application/train_ready.R
+++ application/train_ready.R
@@ -35,7 +35,6 @@ case_var_name = out_nm[y_ind]
 case_dir = paste(case_name, case_var_name, sep = "_")
 
 dir.create(file.path("./cases", case_dir))
-setwd(file.path("./cases", case_dir))
 
 ######################
 wd = "~/Documents/code_proj/simp_add_gp/"
@@ -48,7 +47,7 @@ apply(X = matrix(fun_names, ncol = 1), MARGIN = 1, FUN = source)
 
 ########################################################################################
 # which(!is.na(out[1,])):1  3  4  5  6  7  8  9 10 11 12 15 16 17 22
-
+setwd(file.path("./cases", case_dir))
 ########################
 l1_info = seq_gaus_1d_a(x = inp_trn, y = out_trn[,y_ind], range = range_pre[1], nugget = nugget_pre, iteration = no_single)
 l2_info = seq_gaus_2d(x = inp_trn, y = l1_info[[2]], range = c(range_pre[2], range_pre[2]), nugget = nugget_pre, top_n = no_pairs)
@@ -66,7 +65,10 @@ if(groupthree_flat == 0){
 #########
 val_pred = apply_emu_val(inp_trn, out_trn[,y_ind], meta_data = comb_meta, xtst = inp_val)
 select_ind = plot_val(pred_adding = val_pred, ytrue = out_val[,y_ind], comb_meta, 
-                            title = "Validation", threshold1 = threshold1_pre, threshold2 = threshold2_pre)
+                            title = "Selecting parameters", threshold1 = threshold1_pre, threshold2 = threshold2_pre)
+
+dev.copy(png, filename = "emu_selec_para.png", width = 600, height = 500)
+dev.off()
 
 comb_meta_update = comb_meta[select_ind,]
 ####################################
@@ -76,8 +78,12 @@ tst_pred_short = apply_emu_val(inp_trn_val, out_trn_val[,y_ind], meta_data = com
 tst_pred_tst   = apply_emu_val(inp_trn_val, out_trn_val[,y_ind], meta_data = comb_meta, xtst = inp_tst)
 
 sd_diff_all = plot_res(pred_adding = tst_pred_tst, ytrue = out_tst[,y_ind], comb_meta = comb_meta, title = "Based on all parameters and groups")
-sd_diff_update = plot_res(pred_adding = tst_pred_short, ytrue = out_tst[,y_ind], comb_meta = comb_meta_update, title = "Based on optimized parameters and groups")
+dev.copy(png, filename = "sd_reduce_all_parameters.png", width = 600, height = 500)
+dev.off()
 
+sd_diff_update = plot_res(pred_adding = tst_pred_short, ytrue = out_tst[,y_ind], comb_meta = comb_meta_update, title = "Based on optimized parameters and groups")
+dev.copy(png, filename = "sd_reduce_updated_parameters.png", width = 600, height = 500)
+dev.off()
 
 
 plot(out_tst[,y_ind], tst_pred_short[[1]], col = "navy", pch = 16, cex = 0.7,
@@ -88,6 +94,10 @@ legend("topleft", # Position of the legend
        legend=c("Optimized", "All parameters and groups"), # Labels
        col=c("navy", "red"), # Colors
        pch=c(16, 16)) # Point types
+
+dev.copy(png, filename = "emu_comp.png", width = 400, height = 400)
+dev.off()
+
 ################################
 
 trained_emu = apply_trn(x = inp_trn_val, y = out_trn_val[,y_ind], comb_meta_update)
@@ -100,3 +110,4 @@ saveRDS(trained_emu, file = paste(case_var_name, "emus.R", sep = "_"), ascii = F
 
 write.table(sd_diff_all, file = "sd_reduce_all.csv", col.names = TRUE, row.names = FALSE, sep = ",")
 write.table(sd_diff_update, file = "sd_reduce_update.csv", col.names = TRUE, row.names = FALSE, sep = ",")
+
